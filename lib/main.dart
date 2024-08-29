4d54236458f468f43d9135bb3706b02bfb4f0688
NiBienvenu <nicibienvenu@gmail.com>
2024-08-29 14:53:51 +0200

    Valide Numero

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Valide Numero',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade400),
         useMaterial3: true,
